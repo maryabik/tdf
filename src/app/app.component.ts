528dc1eba4859d65091a3dcb157a30244eeedb77
DESKTOP-JEUJEIQ\Seamfix <bachodo@seamfix.com>
2019-01-14 15:02:22 +0100

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  title = 'Forms';
+
+  topics = ['Angular', 'React', 'Vue'];
+  topicHasError = true;
+
+
+  userModel = new User ('Rob','mary@gmail.com',555-555-7896,'','default','morning', true);
+
+  validateTopic(value) {
+    if(value === 'default'){
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false; 
+    }
+  }
+
 }
---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor(
+        public username: string,
+        public email: string,
+        public phone: number,
+        public password: string,
+        public ntopic: string,
+        public time: string,
+        public subscribe: boolean
+      ) {  }
+}
